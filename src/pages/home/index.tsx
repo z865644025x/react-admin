be80e0cc10b2daf60370c5a4c35b9b205ee8546f
SK-20200629EEAY\Focusmedia <1390516519@qq.com>
2020-07-29 15:04:31 +0800

    添加Card组件

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,7 +1,13 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
 import React from 'react';
+import { Card } from "antd";
 
 export const Home: React.FC = () => {
   return (
-    <div>Home</div>
+    <div>
+      <Card type="inner" title="Inner Card title" extra={<a href="#">More</a>}>
+        Shou
+      </Card>
+    </div>
   )
 }
